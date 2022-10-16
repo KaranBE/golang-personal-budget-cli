5096cb6dc9da1a6517d29c935efb4bec1caf77a1
Karan <karan.bhansali42@gmail.com>
2022-10-16 21:14:47 +0530

    Update budget_1.go

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,12 @@
 package module1
 
 // Budget stores budget information
-
+type Budget struct {
+  Max flaot32
+  Items []Iten
+}
 // Item stores item information
+type Item struct{
+  Description string
+  Price float32
+}
